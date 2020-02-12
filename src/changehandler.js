1d42b1de11bf94d35d1599bd065d2cef4cb1adfa
agnew832 <agnew832@gmail.com>
2020-02-12 09:15:44 -0500

    final commit

--- src/changehandler.js
+++ src/changehandler.js
@@ -7,6 +7,7 @@ class ChangeHandler {
   constructor(amountDue) {
     this.amountDue = amountDue;
     this.cashTendered = 0;
+   
   }
 
   /**
@@ -14,17 +15,43 @@ class ChangeHandler {
    * The parameter "type" is a string that is either quarter, dime, nickel, or penny
    */
   insertCoin(typeOfCoin) {
-
+    if (typeOfCoin === "quarter") {
+      this.cashTendered += 25;
+    } else if (typeOfCoin === "dime") {
+      this.cashTendered += 10;
+    } else if (typeOfCoin === "penny") {
+      this.cashTendered += 1;
+    }
   }
 
   /**
    * Returns true if enough coins have been inserted to at least meet the amountDue
    */
   isPaymentSufficient() {
-      
+    if (this.cashTendered >= this.amountDue) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
+  
   giveChange() {
+   let changeDue = {quarters:0, dimes:0, nickels:0, pennies:0} ,   
+    while (this.cashTendered > this.amountDue) {
+      if ((this.cashTendered - this.amountDue)>= 25) {
+        (this.cashTendered > this.amountDue)-= 25;
+        quarters++;
+      } else if ((this.cashTendered - this.amountDue)>= 10) {
+        (this.cashTendered > this.amountDue) -= 10;
+        dimes++;
+      } else if ((this.cashTendered - this.amountDue) >= 5) {
+        change -= 5;
+        nickels++;
+      } else {
+        ((this.cashTendered > this.amountDue) < 5);
+        pennies++;
+      }
+    }
     // TODO return the correct change in the following format...
     return {
       quarters: 0,
--- tests/changehandler.test.js
+++ tests/changehandler.test.js
@@ -1,11 +1,111 @@
 let { ChangeHandler } = require("../src/changehandler");
 
 describe("Tests for ChangeHandler", function() {
+  test("amountDue is set based on an argument.", function() {
+    let test = new ChangeHandler(1);
+    expect(test.amountDue).toBe(1);
+  });
+  test("cashTendered is set to zero", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin();
+    expect(test.cashTendered).toBe(0);
+  });
+  test("inserting a quarter adds 25", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("quarter");
+    expect(test.cashTendered).toBe(25);
+  });
 
-    // Set up a test below...
-    test("The ChangeHandler class is defined.", function() {
-        // Remember, you can arrange, act, and assert...start small
-        expect(ChangeHandler).toBeDefined();
+  test("inserting a dime adds 10", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("dime");
+    expect(test.cashTendered).toBe(10);
+  });
+
+  test("Inserting a penny adds 1.", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("penny");
+    expect(test.cashTendered).toBe(1);
+  });
+});
+
+describe("isPaymentSufficient", function() {
+  test("Returns true if cashTendered more than amountDue", function() {
+    let test = new ChangeHandler(30);
+    test.insertCoin("quarter");
+    test.insertCoin("quarter");
+    expect(test.isPaymentSufficient()).toBe(true);
+  });
+
+  test("Returns false if cashTendered less than amountDue.", function() {
+    let test = new ChangeHandler(55);
+    test.insertCoin("quarter");
+    test.insertCoin("quarter");
+    expect(test.isPaymentSufficient()).toBe(false);
+  });
+  test("Returns true if cashTendered equal to amountDue", function() {
+    let test = new ChangeHandler(50);
+    test.insertCoin("quarter");
+    test.insertCoin("quarter");
+    expect(test.isPaymentSufficient()).toBe(true);
+  });
+});
+
+describe("giveChange", function() {
+  test("32 change produces: quarters: 1, dimes: 0, nickels: 1, pennies: 2.", function() {
+    let test = new ChangeHandler();
+    test.insertCoin("quarter");
+    test.insertCoin("nickel");
+    test.insertCoin("penny");
+    test.insertCoin("penny");
+    expect(test.giveChange()).toBe({
+      quarters: 1,
+      dimes: 0,
+      nickels: 1,
+      pennies: 2
     });
+  });
+  test("10 change produces: quarters: 0, dimes: 1, nickels: 0, pennies: 0", function() {
+    let test = new ChangeHandler();
+    test.insertCoin("dime");
 
+    expect(test.giveChange()).toBe({
+      quarters: 0,
+      dimes: 1,
+      nickels: 0,
+      pennies: 0
+    });
+  });
+  test("27 change produces: quarters: 1, dimes: 0, nickels: 0, pennies: 2", function() {
+    let test = new ChangeHandler(27);
+    test.insertCoin("quarter");
+    test.insertCoin("quarter");
+    test.insertCoin("penny");
+    test.insertCoin("penny");
+    test.insertCoin("penny");
+    test.insertCoin("penny");
+    expect(test.giveChange()).toBe({
+      quarters: 1,
+      dimes: 0,
+      nickels: 0,
+      pennies: 2
+    });
+  });
+
+  test("68 change produces: quarters: 2, dimes: 1, nickels: 1, pennies: 3", function() {
+    let test = new ChangeHandler(68);
+    test.insertCoin("quarter");
+    test.insertCoin("quarter");
+    test.insertCoin("dime");
+    test.insertCoin("nickel");
+    test.insertCoin("penny");
+    test.insertCoin("penny");
+    test.insertCoin("penny");
+    expect(test.giveChange()).toBe({
+      quarters: 2,
+      dimes: 1,
+      nickels: 1,
+      pennies: 3
+    });
+  });
 });
